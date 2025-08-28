0a3f67a1cc331d2a2feeac817e700ff349d7b54e
Uday-Shah-009 <udayshah.24.ce@iite.indusuni.ac.in>
2025-08-28 11:30:55 +0530

    Add error handling to GetAnalisys API

--- src/Services/GetAnalisys.js
+++ src/Services/GetAnalisys.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 export const GetAnalisys = async (message) => {
-  const data = await axios.post("https://sms-api-2-ptk8.onrender.com/predict", {message: message})
-  console.log(data);
-  return data.data;
+  try {
+    const response = await axios.post(
+      "https://sms-api-2-ptk8.onrender.com/predict",
+      { message }
+    );
+    console.log("Flask Response:", response.data);
+    return response.data;
+  } catch (err) {
+    console.error("Error calling Flask API:", err.message);
+    throw err;
+  }
 };

7d831ce93da9c2b07ad4c2fa3d60b71317be1a60
Uday-Shah-009 <udayshah.24.ce@iite.indusuni.ac.in>
2025-08-28 10:22:55 +0530

    Update CORS origin for the server

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://stay-alert-tev4.vercel.app",
+    origin: "https://stayalertsms.netlify.app",
     methods: ["GET", "POST"],
   })
 );
